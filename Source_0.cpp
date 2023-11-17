bf9a6f520823eb0192ce4d401a5a5ac89322e925
ch1ll1xxc <126010426+ch1ll1xxc@users.noreply.github.com>
2023-11-17 20:34:25 +0300

    Add files via upload

---
+++ Source_0.cpp
@@ -0,0 +1,15 @@
+#include "Header_0.h"
+
+using namespace siaod_7;
+
+int main() {
+	fano::help();
+	system("pause");
+	system("cls");
+	LZ77::help();
+	system("pause");
+	system("cls");
+	LZ78::help();
+	system("pause");
+	system("exit");
+}
